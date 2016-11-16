da1f5742b418db934394822388a723e81341bef3
Italo Silva <italo@maleldil.com>
2016-11-16 12:49:46 -0200

    Adiciona comentários em simulacao.c

--- src/simulacao.c
+++ src/simulacao.c
@@ -3,6 +3,10 @@
 #include "constantes.h"
 #include "rand.h"
 
+/**
+ * Define o estado da simulação. Contém o cenário atual (o tabuleiro), as
+ * coordenadas x e y do robô (pos_x e y), assim como a pontuação acumulada.
+ */
 typedef struct
 {
   Conteudo tabuleiro[TAM_GRID][TAM_GRID];
@@ -11,6 +15,9 @@ typedef struct
   int pontuacao;
 } Estado;
 
+/**
+ * Imprime o tabuleiro como uma matriz de inteiros.
+ */
 static inline void
 print_tabuleiro(Conteudo tabuleiro[][TAM_GRID])
 {
@@ -23,9 +30,13 @@ print_tabuleiro(Conteudo tabuleiro[][TAM_GRID])
   printf("\n");
 }
 
+/**
+ * Inicializa o tabuleiro aleatoriamente.
+ */
 static inline void
 init_tabuleiro(Conteudo tabuleiro[][TAM_GRID], unsigned long* seed)
 {
+  // Inicializa as bordas como paredes.
   for (int i = 0; i < TAM_GRID; i++) {
     tabuleiro[i][0] = PAREDE;
     tabuleiro[0][i] = PAREDE;
@@ -33,6 +44,7 @@ init_tabuleiro(Conteudo tabuleiro[][TAM_GRID], unsigned long* seed)
     tabuleiro[TAM_GRID - 1][i] = PAREDE;
   }
 
+  // Percorre as outras células, que terão 50% de chance de conter uma lata.
   for (int i = 1; i < TAM_GRID - 1; i++) {
     for (int j = 1; j < TAM_GRID - 1; j++) {
       double x = myrand(seed) / (double)MYRAND_MAX;
@@ -41,6 +53,10 @@ init_tabuleiro(Conteudo tabuleiro[][TAM_GRID], unsigned long* seed)
   }
 }
 
+/**
+ * Inicializa a simulação. O tabuleiro é gerado, o robô é colocado na origem
+ * (que é 1,1 porque 0,0 é uma parede) e a pontuação é zerada.
+ */
 static inline void
 init_estado(Estado* e, unsigned long* seed)
 {
@@ -49,17 +65,33 @@ init_estado(Estado* e, unsigned long* seed)
   e->pontuacao = 0;
 }
 
+/**
+ * Obtém o índice linear do cenário a partir dos conteúdos das células
+ * adjacentes ao robô. Esse índice será utilizado para consultar o movimento a
+ * ser executado pelo robô, tal qual descrito no cromossomo.
+ */
 static inline int
 get_cenario(Estado* e)
 {
-  int norte = e->tabuleiro[e->pos_y - 1][e->pos_x];
-  int oeste = e->tabuleiro[e->pos_y][e->pos_x - 1];
-  int atual = e->tabuleiro[e->pos_y][e->pos_x];
-  int leste = e->tabuleiro[e->pos_y][e->pos_x + 1];
-  int sul = e->tabuleiro[e->pos_y + 1][e->pos_x];
+  // Cada uma das células adjacentes possui um conteúdo, representado por 0, 1
+  // ou 2. Os 5 algarismos formam um número em base 3, que será convertido para
+  // base 10 e retornado.
+  Conteudo norte = e->tabuleiro[e->pos_y - 1][e->pos_x];
+  Conteudo oeste = e->tabuleiro[e->pos_y][e->pos_x - 1];
+  Conteudo atual = e->tabuleiro[e->pos_y][e->pos_x];
+  Conteudo leste = e->tabuleiro[e->pos_y][e->pos_x + 1];
+  Conteudo sul = e->tabuleiro[e->pos_y + 1][e->pos_x];
 
   return 81 * norte + 27 * sul + 9 * leste + 3 * oeste + atual;
 }
+
+/**
+ * Funções de movimentação
+ *
+ * Recebem um estado e tentam se movimentar em uma direção. Se naquela direção
+ * existir uma parede, a penalidade é aplicada. Senão, a coordenada é
+ * modificada.
+ */
 
 static inline void
 move_norte(Estado* e)
@@ -101,6 +133,10 @@ move_oeste(Estado* e)
   }
 }
 
+/**
+ * Tenta pegar uma lata na célula atual do robô. Se não existir uma, sofre a
+ * penalidade. Se existir, esvazia a célula e adicionada a pontuação.
+ */
 static inline void
 pega_lata(Estado* e)
 {
@@ -112,10 +148,14 @@ pega_lata(Estado* e)
   }
 }
 
+// Protótpos das funções, uma vez que elas chamam uma à outra.
 static inline void proximo_estado(Estado* e, int movimento,
                                   unsigned long* seed);
 static inline void move_aleatorio(Estado* e, unsigned long* seed);
 
+/**
+ * Obtém um movimento aleatório e o executa.
+ */
 void
 move_aleatorio(Estado* e, unsigned long* seed)
 {
@@ -123,6 +163,9 @@ move_aleatorio(Estado* e, unsigned long* seed)
   proximo_estado(e, movimento, seed);
 }
 
+/**
+ * Executa um Movimento, modificando o Estado.
+ */
 void
 proximo_estado(Estado* e, int movimento, unsigned long* seed)
 {
